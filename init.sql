25a3015085bcdaafc98ed8e7993d0aebb4869507
Александр <fridrix.aleksandr.2004@mail.ru>
2024-11-18 21:18:28 +0300

    starting projects_service

--- init.sql
+++ init.sql
@@ -1,4 +1,6 @@
 -- For users service
-CREATE USER users_service WITH PASSWORD 'root';
-CREATE DATABASE users ENCODING 'UTF-8' OWNER users_service;
-GRANT all privileges on DATABASE users to users_service;
+CREATE USER jir WITH PASSWORD 'root';
+CREATE DATABASE users ENCODING 'UTF-8' OWNER jir;
+CREATE DATABASE projects_service ENCODING 'UTF-8' OWNER jir;
+GRANT all privileges on DATABASE users to jir;
+GRANT all privileges on DATABASE projects_service to jir;
